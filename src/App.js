c068345f937c88a8fd587482278769949b9330a3
Rahul Meka <rahul.meka@pens.com>
2022-09-13 03:23:46 +0530

    minor

--- src/App.js
+++ src/App.js
@@ -5,54 +5,54 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://live.staticflickr.com/65535/52332920296_fe8fca84b4_h.jpg" width={500} height={500} />
-        <img src="https://www.gstatic.com/webp/gallery/2.jpg" alt="Picture of the author"
+        <img src="https://live.staticflickr.com/65535/52332920296_fe8fca84b4_h.jpg" width={500} height={500} alt="csr of the author" />
+        <img src="https://www.gstatic.com/webp/gallery/2.jpg" alt="csr of the author"
           width={500}
           height={500} />
-        <img src="https://www.gstatic.com/webp/gallery/3.jpg" alt="Picture of the author"
+        <img src="https://www.gstatic.com/webp/gallery/3.jpg" alt="csr of the author"
           width={500}
           height={500} />
-        <img src="https://www.gstatic.com/webp/gallery/4.jpg" alt="Picture of the author"
-          width={500}
-          height={500} />
-
-        <img src="https://www.gstatic.com/webp/gallery/2.jpg" alt="Picture of the author"
-          width={500}
-          height={500} />
-        <img src="https://www.gstatic.com/webp/gallery/3.jpg" alt="Picture of the author"
-          width={500}
-          height={500} />
-        <img src="https://www.gstatic.com/webp/gallery/4.jpg" alt="Picture of the author"
+        <img src="https://www.gstatic.com/webp/gallery/4.jpg" alt="csr of the author"
           width={500}
           height={500} />
 
-        <img src="https://www.gstatic.com/webp/gallery/2.jpg" alt="Picture of the author"
+        <img src="https://www.gstatic.com/webp/gallery/2.jpg" alt="csr of the author"
           width={500}
           height={500} />
-        <img src="https://www.gstatic.com/webp/gallery/3.jpg" alt="Picture of the author"
+        <img src="https://www.gstatic.com/webp/gallery/3.jpg" alt="csr of the author"
           width={500}
           height={500} />
-        <img src="https://www.gstatic.com/webp/gallery/4.jpg" alt="Picture of the author"
-          width={500}
-          height={500} />
-
-        <img src="https://www.gstatic.com/webp/gallery/2.jpg" alt="Picture of the author"
-          width={500}
-          height={500} />
-        <img src="https://www.gstatic.com/webp/gallery/3.jpg" alt="Picture of the author"
-          width={500}
-          height={500} />
-        <img src="https://www.gstatic.com/webp/gallery/4.jpg" alt="Picture of the author"
+        <img src="https://www.gstatic.com/webp/gallery/4.jpg" alt="csr of the author"
           width={500}
           height={500} />
 
-        <img src="https://www.gstatic.com/webp/gallery/2.jpg" alt="Picture of the author"
+        <img src="https://www.gstatic.com/webp/gallery/2.jpg" alt="csr of the author"
           width={500}
           height={500} />
-        <img src="https://www.gstatic.com/webp/gallery/3.jpg" alt="Picture of the author"
+        <img src="https://www.gstatic.com/webp/gallery/3.jpg" alt="csr of the author"
           width={500}
           height={500} />
-        <img src="https://www.gstatic.com/webp/gallery/4.jpg" alt="Picture of the author"
+        <img src="https://www.gstatic.com/webp/gallery/4.jpg" alt="csr of the author"
+          width={500}
+          height={500} />
+
+        <img src="https://www.gstatic.com/webp/gallery/2.jpg" alt="csr of the author"
+          width={500}
+          height={500} />
+        <img src="https://www.gstatic.com/webp/gallery/3.jpg" alt="csr of the author"
+          width={500}
+          height={500} />
+        <img src="https://www.gstatic.com/webp/gallery/4.jpg" alt="csr of the author"
+          width={500}
+          height={500} />
+
+        <img src="https://www.gstatic.com/webp/gallery/2.jpg" alt="csr of the author"
+          width={500}
+          height={500} />
+        <img src="https://www.gstatic.com/webp/gallery/3.jpg" alt="csr of the author"
+          width={500}
+          height={500} />
+        <img src="https://www.gstatic.com/webp/gallery/4.jpg" alt="csr of the author"
           width={500}
           height={500} />
       </header>
